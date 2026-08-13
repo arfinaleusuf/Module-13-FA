9ec5ba7cfd15cba7eb3b2ff307b0542633af92e8
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-13 22:46:12 +0600

    Update database.py to define SQLALCHEMY_DATABASE_URL for SQLite and comment out MySQL connection

--- database.py
+++ database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/TodoAplicationDatabase'   --- for postgresql
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:password@127.0.0.1:3306/todoaplicationdatabase' 
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:password@127.0.0.1:3306/todoaplicationdatabase' 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
