97aeeecc69b56a723b5484dd2ecf177f19713732
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-14 01:46:31 +0600

    Fix typo in PostgreSQL connection string in database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/TodoAplicationDatabase'   --- for postgresql
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.rqplhotetgjylmkwgrqe:arfin71banglasesh@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.rqplhotetgjylmkwgrqe:arfin71bangladesh@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:password@127.0.0.1:3306/todoaplicationdatabase' 
 
