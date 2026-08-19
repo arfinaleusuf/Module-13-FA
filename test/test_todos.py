7972f76e21d8b9e78bda0e97f79f02cc64bec512
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-19 23:30:09 +0600

    Update test_read_todos to expect HTTP 200 OK status

--- test/test_todos.py
+++ test/test_todos.py
@@ -13,5 +13,5 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 def test_read_todos():
     respons = client.get('/')
-    assert respons.status_code == status.HTTP_401_UNAUTHORIZED
+    assert respons.status_code == status.HTTP_200_OK
     
