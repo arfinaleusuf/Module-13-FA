2823701c0ee2080db92e11a140e3e994e91e0233
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-19 23:41:26 +0600

    Add test for creating a todo item with mock database session

--- test/test_todos.py
+++ test/test_todos.py
@@ -2,12 +2,27 @@ from test.test_main import client
 from main import app
 from fastapi import status
 from router.auth import get_current_user
+from database import SessionLocal
+from models import Todos
 
 def override_get_current_user():
     return {
         'id' : 1,
         'usename': 'testuser'
     }
+
+def test_todo():
+    db = SessionLocal()
+
+    todo = Todos(
+        id = 101,
+        title ='Testing',
+        description = 'Testing',
+        priority = 5,
+        completed = True,
+        owner_id = 1
+    )
+
 
 app.dependency_overrides[get_current_user] = override_get_current_user
 
